4da19c3146bd789d1f4dfb169cbf785f0e0cfcfa
Aakash <aakashgaurav456@gmail.com>
2023-02-25 08:03:16 +0530

    fw22_0141 Day4

--- chat/backend/chat.js
+++ chat/backend/chat.js
@@ -4,11 +4,12 @@ const app = express();
 require("dotenv").config();
 const http = require("http");
 const http_server= http.createServer(app);
+const moment = require("moment");
 
 const io = socketio(http_server);
 io.on("connection",(socket)=>{
     socket.on("msg_from_client",(msg)=>{ 
-        msg.time = new Date;
+        msg.time = moment().format(`h:mm a`);
         socket.broadcast.emit("msg_from_server",msg);
         socket.emit("msg_from_server",msg);
     })
